0026b23fb87101ac3ac6b8acfe1164e34469d7c6
HiteshG1995 <Hiteshghadage@gmail.com>
2022-12-08 16:26:19 +0530

    variable Learning JavaScript.
    Use of variable.

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName; //Variable declaration
+myName = "Hitesh Ghadage"; // Variable Initilization
+console.log(myName);
+
+var myFriend = "New Name" ; //Variable decleration and initilization at the same time
+console.log(myFriend);
+
+myFriend = "Update name"; //Variable update
+console.log(myFriend);
---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+var myFullName;
+console.log(myFullName); 
+
+var myFullName;
+myFullName="Hitesh Ghadage";
+console.log(myFullName);
+
+myFullName="Hitesh Prashant Ghadage";
+console.log(myFullName);
+
+var myLove="I Love only JS";
+console.log(myLove);
+
+var firstName, lastName, age, address, pin_code, gender, is_married;
+firstName="Hitesh";
+lastName="Ghadage";
+age="27";
+address="Dehuroad";
+pin_code="412101";
+gender="male";
+is_married="No";
+console.log(firstName, lastName, age, address, pin_code, gender, is_married);
+
+
