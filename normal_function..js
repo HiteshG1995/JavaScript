f9fd7092e72d02be62264a8ec6feb20e9e383e5a
HiteshG1995 <Hiteshghadage@gmail.com>
2022-12-11 16:54:42 +0530

    Normal Function
    Normal Function

---
+++ normal_function..js
@@ -0,0 +1,8 @@
+function bankDetails(bankName, accountNumber, location, pinCode){
+    console.log("|Bank name is: ",bankName,"|Account number is: ", accountNumber,"|Branch location: " ,location,"|Bank Pincode", pinCode);
+}
+console.log("-------------------------------------------Bank Details--------------------------------------------------")
+bankDetails("CITI Bank",3456782345,'Pune',431202);
+bankDetails("Axis Bank",7856782345,'Mumbai',441202);
+bankDetails("HDFC Bank",8956782345,'Pune',631202);
+console.log("---------------------------------------------------------------------------------------------------------")
