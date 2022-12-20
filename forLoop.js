27cffb4b02159a332caf6809b801433280778b02
HiteshG1995 <Hiteshghadage@gmail.com>
2022-12-20 18:53:06 +0530

    For loop and switchCase Assignment
    For loop and switchCase Assignment

---
+++ forLoop.js
@@ -0,0 +1,41 @@
+console.log(`Print 5 to 15\n`);
+
+for (let index = 5; index < 16; index=index+1) {
+    console.log(index);    
+}
+
+console.log(`\nPrint 50 to 40\n`);
+
+for (let index = 50; index > 39; index=index-1) {
+    console.log(index);    
+}
+
+console.log(`\nPrint first 15 odd number\n`);
+
+for (let index = 1; index < 30; index=index+=2) {
+    console.log(index);    
+}
+
+console.log(`\nPrint first 15 even number\n`);
+
+for (let index = 0; index < 30; index=index+2) {
+    console.log(index);    
+}
+
+console.log(`\nPrint table from 5 to 50\n`);
+
+for (let index = 5; index < 55; index=index+5) {
+    console.log(index);    
+}
+
+console.log(`\nPrint table from 10 to 100\n`);
+
+for (let index = 10; index < 110; index=index+10) {
+    console.log(index);    
+}
+
+console.log(`\nPrint reverse from 100 to 10\n`);
+
+for (let index = 100; index > 0; index=index-10) {
+    console.log(index);    
+}
